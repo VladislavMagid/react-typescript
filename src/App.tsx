1d2632e27613b2fbf6b9ed44514f8d8e1559cb33
VladislavMagid <Vladislav.magid@gmail.com>
2024-07-03 22:10:03 +0200

    Lesson 6

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
+//Lessons
+import Lesson06 from "./lessons/Lesson06";
 
 function App() {
-  return <div className="App">React TypeScript App</div>;
+  return (
+    <div className="App">
+      <Lesson06 />
+    </div>
+  );
 }
 
 export default App;
