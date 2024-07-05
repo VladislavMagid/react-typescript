2feff55ae93da1fd7e3824c238646d07070b16db
VladislavMagid <Vladislav.magid@gmail.com>
2024-07-05 16:26:26 +0200

    Prettier

--- src/homeworks/Homework_06/Homework_06.tsx
+++ src/homeworks/Homework_06/Homework_06.tsx
@@ -2,11 +2,11 @@ import "./styles.css";
 import { v4 } from "uuid";
 
 function Homework_06() {
-    interface Car {
-        brand: string;
-        price: number;
-        isDiesel: boolean;
-    }
+  interface Car {
+    brand: string;
+    price: number;
+    isDiesel: boolean;
+  }
   const cars: Car[] = [
     { brand: "BMW", price: 20000, isDiesel: true },
     { brand: "Mercedes", price: 22000, isDiesel: false },
@@ -15,18 +15,16 @@ function Homework_06() {
     { brand: "Audi", price: 50000, isDiesel: true },
   ];
 
-  const cards = cars.map((car)=> {
-    return(
-        <div className="cars-cards" key={v4()}>
-            <p>Brand: {car.brand}</p>
-            <p>Price: {car.price}</p>
-            <p>Diesel: {car.isDiesel? "yes" : "no"}</p>
-        </div>
-    )
-  })
-  return <div className="page-wrapper">
-    {cards}
-  </div>;
+  const cards = cars.map((car) => {
+    return (
+      <div className="cars-cards" key={v4()}>
+        <p>Brand: {car.brand}</p>
+        <p>Price: {car.price}</p>
+        <p>Diesel: {car.isDiesel ? "yes" : "no"}</p>
+      </div>
+    );
+  });
+  return <div className="page-wrapper">{cards}</div>;
 }
 
 export default Homework_06;
