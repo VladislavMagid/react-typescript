ff7ee8f10549b2076c0d7cfac32d7a29290a849c
VladislavMagid <Vladislav.magid@gmail.com>
2024-07-15 15:20:28 +0200

    Lesson 10 Project

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Lesson_06 from "./lessons/Lesson_06/Lesson_06";
 import Lesson_07 from "./lessons/Lesson_07/Lesson_07";
 import Lesson_08 from "lessons/Lesson_08/Lesson_08";
 import Lesson_09 from "lessons/Lesson_09/Lesson_09";
-
+import Lesson_10 from "lessons/Lesson_10/Lesson_10";
 
 // Homeworks
 import Homework_06 from "./homeworks/Homework_06/Homework_06";
@@ -24,8 +24,8 @@ function App() {
       {/* <Homework_06 /> */}
       {/* <Homework_07 /> */}
       {/* <Homework_08/> */}
-      <Homework_09/>
-  
+      {/* <Homework_09/> */}
+      <Lesson_10/>
     </>
   );
 }
